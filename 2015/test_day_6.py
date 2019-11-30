5b2d3593036612c11e793519a921767894141fb1
John Vano <johnvano@gmail.com>
2019-11-30 10:08:49 -0800

    Fixed day 6 formatting

--- 2015/test_day_6.py
+++ 2015/test_day_6.py
@@ -29,46 +29,48 @@ class Lights:
     After following the instructions, how many lights are lit?
     """
 
-    grid = np.zeros((1000,1000), dtype=int)
+    grid = np.zeros((1000, 1000), dtype=int)
 
-    def f_on(self,x):
+    def f_on(self, x):
         return 1
 
-    def f_off(self,x):
+    def f_off(self, x):
         return 0
 
-    def f_toggle(self,x):
-        return 1-x
+    def f_toggle(self, x):
+        return 1 - x
 
     def count(self):
         return sum(sum(self.grid))
 
-    def on(self,ul,lr):
-        for x in range(ul[0],lr[0]+1):
-            for y in range(ul[1],lr[1]+1):
+    def on(self, ul, lr):
+        for x in range(ul[0], lr[0] + 1):
+            for y in range(ul[1], lr[1] + 1):
                 self.grid[x][y] = self.f_on(self.grid[x][y])
 
-    def off(self,ul,lr):
-        for x in range(ul[0],lr[0]+1):
-            for y in range(ul[1],lr[1]+1):
+    def off(self, ul, lr):
+        for x in range(ul[0], lr[0] + 1):
+            for y in range(ul[1], lr[1] + 1):
                 self.grid[x][y] = self.f_off(self.grid[x][y])
 
-    def toggle(self,ul,lr):
-        for x in range(ul[0],lr[0]+1):
-            for y in range(ul[1],lr[1]+1):
+    def toggle(self, ul, lr):
+        for x in range(ul[0], lr[0] + 1):
+            for y in range(ul[1], lr[1] + 1):
                 self.grid[x][y] = self.f_toggle(self.grid[x][y])
 
-    def execute_direction(self,direction):
-        m = re.match(r"(?P<action>turn on|turn off|toggle) (?P<start_x>\d+),(?P<start_y>\d+) through (?P<end_x>\d+),(?P<end_y>\d+)",direction)
+    def execute_direction(self, direction):
+        m = re.match(
+            r"(?P<action>turn on|turn off|toggle) (?P<start_x>\d+),(?P<start_y>\d+) through (?P<end_x>\d+),(?P<end_y>\d+)",
+            direction)
         if m:
             ul = (int(m.group('start_x')), int(m.group('start_y')))
-            lr = (int(m.group('end_x')),int(m.group('end_y')))
+            lr = (int(m.group('end_x')), int(m.group('end_y')))
             if m.group('action') == 'toggle':
-                self.toggle(ul,lr)
+                self.toggle(ul, lr)
             elif m.group('action') == 'turn off':
-                self.off(ul,lr)
+                self.off(ul, lr)
             elif m.group('action') == 'turn on':
-                self.on(ul,lr)
+                self.on(ul, lr)
 
     def execute_directions(self):
         with open('./input_day_6.txt', 'r') as fp:
@@ -81,38 +83,38 @@ class Lights:
 def test_lights():
     lights = Lights()
     assert lights.count() == 0
-    lights.on((0,0),(3,5))
+    lights.on((0, 0), (3, 5))
     assert lights.count() == 24
-    lights.off((1,1),(2,2))
+    lights.off((1, 1), (2, 2))
     assert lights.count() == 20
-    lights.toggle((0,0),(1,1))
+    lights.toggle((0, 0), (1, 1))
     assert lights.count() == 18
-    lights.off((0,0),(999,999))
+    lights.off((0, 0), (999, 999))
     assert lights.count() == 0
     lights.execute_direction('turn on 0,0 through 3,5')
     assert lights.count() == 24
+
 
 def test_submit():
     lights = Lights()
     lights.execute_directions()
     assert lights.count() == 543903
 
+
 class NewLights(Lights):
     pass
 
-    def f_on(self,x):
-        return x+1
+    def f_on(self, x):
+        return x + 1
 
-    def f_off(self,x):
-        return max(x-1,0)
+    def f_off(self, x):
+        return max(x - 1, 0)
 
-    def f_toggle(self,x):
-        return x+2
+    def f_toggle(self, x):
+        return x + 2
+
 
 def test_new_lights():
     lights = NewLights()
     lights.execute_directions()
     assert lights.count() == 14687245
-
-
-
