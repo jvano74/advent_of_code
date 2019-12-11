ff9f9e1a31683556dad21a9550c1f922093484b6
John Vano <johnvano@gmail.com>
2019-12-10 21:58:33 -0800

    2019 Day 10

---
+++ 2019/test_day_10.py
@@ -0,0 +1,78 @@
+from typing import List
+
+
+def normalize_vector(delta):
+    if delta[0] > 0:
+        return 1, delta[1] / delta[0]
+    if delta[0] < 0:
+        return -1, -delta[1] / delta[0]
+    if delta[1] > 0:
+        return 0, 1
+    if delta[1] < 0:
+        return 0, -1
+    return 0, 0
+
+
+def test_normalize_vector():
+    assert normalize_vector((0,5)) == (0,1.0)
+    assert normalize_vector((0,-3)) == (0,-1.0)
+    assert normalize_vector((2,2)) == (1,1.0)
+    assert normalize_vector((1,-2)) == (1,-2.0)
+    assert normalize_vector((-2,1)) == (-1,0.5)
+
+
+def count_directions(deltas: List):
+    return len(set([normalize_vector(d) for d in deltas]))
+
+
+def test_count_directions():
+    assert count_directions([(0,1),(0,2)]) == 1
+    assert count_directions([(0,1),(0,-2)]) == 2
+    assert count_directions([(-1,1),(0,1),(1,1),
+                             (-1,0),(0,0),(1,0),
+                             (-1,-1),(0,-1),(1,-1)]) == 9
+
+
+def find_deltas(x, map):
+    return [ (k[0] - x[0], k[1] - x[1]) for k in map ]
+
+
+def test_find_deltas():
+    map = {(0, 0), (1, 0), (0, 1), (1, 1), }
+    assert len(find_deltas((0, 0), map)) == 4
+    assert count_directions(find_deltas((0, 0), map)) == 4
+
+
+def build_map(map_str):
+    map_dict = {}
+    for y, line in enumerate(map_str.split('\n')):
+        for x, c in enumerate(line):
+            if c == '#':
+                map_dict[(x,y)] = 1
+    return map_dict
+
+
+def test_build_map():
+    map_str = '.#..#\n.....\n#####\n....#\n...##'
+    map_dict = {(0, 2): 1, (1, 0): 1, (1, 2): 1, (2, 2): 1, (3, 2): 1,
+                (3, 4): 1, (4, 0): 1, (4, 2): 1, (4, 3): 1, (4, 4): 1}
+    assert build_map(map_str) == map_dict
+
+
+def eval_map(map):
+    map_with_numbers = {k: count_directions(find_deltas(k, map)) for k in map}
+    max_loc = max(map_with_numbers.keys(), key=(lambda k: map_with_numbers[k]))
+    return map_with_numbers, max_loc
+
+def test_eval_map():
+    map_str = '.#..#\n.....\n#####\n....#\n...##'
+    map_with_numbers = {(0, 2): 7, (1, 0): 8,  (1, 2): 8,  (2, 2): 8,  (3, 2): 8,
+                        (3, 4): 9, (4, 0): 8,  (4, 2): 6,  (4, 3): 8,  (4, 4): 8}
+    assert eval_map(build_map(map_str)) == (map_with_numbers, (3, 4))
+
+def test_submission():
+    with open('input_day_10.txt') as fp:
+        map_str = fp.read()
+    map = eval_map(build_map(map_str))
+    assert map[1] == (28, 29)
+    assert map[0][28,29] == 341 - 1
