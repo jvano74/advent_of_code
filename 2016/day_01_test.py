7599633aead4fedb7141bdb0a1ad1618594748c2
John Vano <jvano74@gmail.com>
2021-01-01 16:26:27 -0800

    AoC 2016 day 1 - cleaned up formatting

--- 2016/day_01_test.py
+++ 2016/day_01_test.py
@@ -1,17 +1,27 @@
 import pytest
 
+
 class Problem:
     """
     --- Day 1: No Time for a Taxicab ---
-    Santa's sleigh uses a very high-precision clock to guide its movements, and the clock's oscillator is regulated by stars. Unfortunately, the stars have been stolen... by the Easter Bunny. To save Christmas, Santa needs you to retrieve all fifty stars by December 25th.
+    Santa's sleigh uses a very high-precision clock to guide its movements, and the clock's oscillator is regulated by
+    stars. Unfortunately, the stars have been stolen... by the Easter Bunny. To save Christmas, Santa needs you to
+    retrieve all fifty stars by December 25th.
 
-    Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+    Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second
+    puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
 
-    You're airdropped near Easter Bunny Headquarters in a city somewhere. "Near", unfortunately, is as close as you can get - the instructions on the Easter Bunny Recruiting Document the Elves intercepted start here, and nobody had time to work them out further.
+    You're airdropped near Easter Bunny Headquarters in a city somewhere. "Near", unfortunately, is as close as you
+    can get - the instructions on the Easter Bunny Recruiting Document the Elves intercepted start here, and nobody
+    had time to work them out further.
 
-    The Document indicates that you should start at the given coordinates (where you just landed) and face North. Then, follow the provided sequence: either turn left (L) or right (R) 90 degrees, then walk forward the given number of blocks, ending at a new intersection.
+    The Document indicates that you should start at the given coordinates (where you just landed) and face North.
+    Then, follow the provided sequence: either turn left (L) or right (R) 90 degrees, then walk forward the given
+    number of blocks, ending at a new intersection.
 
-    There's no time to follow such ridiculous instructions on foot, though, so you take a moment and work out the destination. Given that you can only walk on the street grid of the city, how far is the shortest path to the destination?
+    There's no time to follow such ridiculous instructions on foot, though, so you take a moment and work out the
+    destination. Given that you can only walk on the street grid of the city, how far is the shortest path to the
+    destination?
 
     For example:
 
@@ -21,9 +31,11 @@ class Problem:
     How many blocks away is Easter Bunny HQ?
 
     --- Part Two ---
-    Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually at the first location you visit twice.
+    Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually
+    at the first location you visit twice.
 
-    For example, if your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away, due East.
+    For example, if your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away,
+    due East.
 
     How many blocks away is the first location you visit twice?
     """
@@ -35,11 +47,11 @@ with open('day_01_input.txt') as f:
         INPUTS = line.strip().split(', ')
 
 
-def navigate(directions, check_overlap = False):
-    deltas = [(0,1), (1,0), (0,-1), (-1,0)]
+def navigate(directions, check_overlap=False):
+    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     orientation = 0
-    x,y = (0,0)
-    walked = set([(x,y)])
+    x, y = (0, 0)
+    walked = {(x, y)}
     for step in directions:
         if step[0] == 'L':
             orientation -= 1
@@ -69,5 +81,5 @@ def test_inputs():
 
     with pytest.raises(Exception):
         assert navigate(['R2', 'L3'], True)
-    assert navigate(['R8', 'R4', 'R4', 'R8'],True) == 4
-    assert navigate(INPUTS,True) == 158
+    assert navigate(['R8', 'R4', 'R4', 'R8'], True) == 4
+    assert navigate(INPUTS, True) == 158
