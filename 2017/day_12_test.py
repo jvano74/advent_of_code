be643840d0a28ce865d1710ad07bd157ebd9ba27
John Vano <jvano74@gmail.com>
2021-01-04 15:35:54 -0800

    AoC 2017 day 12

--- 2017/day_12_test.py
+++ 2017/day_12_test.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Puzzle:
     """
     --- Day 12: Digital Plumber ---
@@ -38,6 +41,72 @@ class Puzzle:
     Therefore, a total of 6 programs are in this group; all but program 1, which has a pipe that connects it to itself.
 
     How many programs are in the group that contains program ID 0?
+
+    --- Part Two ---
+    There are more programs than just the ones in the group containing program ID 0. The rest of them have no way of
+    reaching that group, and still might have no way of reaching each other.
+
+    A group is a collection of programs that can all communicate via pipes either directly or indirectly. The programs
+    you identified just a moment ago are all part of the same group. Now, they would like you to determine the total
+    number of groups.
+
+    In the example above, there were 2 groups: one consisting of programs 0,2,3,4,5,6, and the other consisting
+    solely of program 1.
+
+    How many groups are there in total?
     """
     pass
 
+
+SAMPLE = ['0 <-> 2', '1 <-> 1', '2 <-> 0, 3, 4', '3 <-> 2, 4',
+          '4 <-> 2, 3, 6', '5 <-> 6', '6 <-> 4, 5']
+
+
+with open('day_12_input.txt') as fp:
+    INPUTS = [line.strip() for line in fp]
+
+
+def parse_input(raw_data):
+    graph = defaultdict(set)
+    for raw_line in raw_data:
+        program, raw_dest = raw_line.split(' <-> ')
+        graph[int(program)] = set(int(p) for p in raw_dest.split(', '))
+    return graph
+
+
+def get_component(graph, node):
+    to_check = set()
+    elements = {node}
+    to_check = to_check.union(graph[node])
+    while len(to_check) > 0:
+        current_node = to_check.pop()
+        elements.add(current_node)
+        for test_node in graph[current_node]:
+            if test_node not in elements:
+                elements.add(test_node)
+                to_check = to_check.union(graph[test_node])
+    return elements
+
+
+def get_groups(graph):
+    groups = set()
+    all_elements = {k for k in graph}
+    while len(all_elements) > 0:
+        next_node = all_elements.pop()
+        component = get_component(graph, next_node)
+        groups.add(frozenset(component))
+        all_elements = all_elements - component
+    return groups
+
+
+def test_sample():
+    sample_graph = parse_input(SAMPLE)
+    assert sample_graph == {0: {2}, 1: {1}, 2: {0, 3, 4}, 3: {2, 4}, 4: {2, 3, 6}, 5: {6}, 6: {4, 5}}
+    assert get_component(sample_graph, 0) == {0, 2, 3, 4, 5, 6}
+    assert get_groups(sample_graph) == {frozenset({1}), frozenset({0, 2, 3, 4, 5, 6})}
+
+
+def test_puzzle():
+    puzzle_graph = parse_input(INPUTS)
+    assert len(get_component(puzzle_graph, 0)) == 288
+    assert len(get_groups(puzzle_graph)) == 211
