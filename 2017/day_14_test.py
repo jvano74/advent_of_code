583dc1a334a639adf44b96d138585cc70db473ff
John Vano <jvano74@gmail.com>
2021-01-04 17:23:49 -0800

    AoC 2017 day 14

--- 2017/day_14_test.py
+++ 2017/day_14_test.py
@@ -1,3 +1,6 @@
+from typing import NamedTuple
+
+
 class Puzzle:
     """
     --- Day 14: Disk Defragmentation ---
@@ -41,6 +44,136 @@ class Puzzle:
     Given your actual key string, how many squares are used?
 
     Your puzzle input is wenycdww.
+
+    --- Part Two ---
+    Now, all the defragmenter needs to know is the number of regions. A region is a group of used squares that are all
+    adjacent, not including diagonals. Every used square is in exactly one region: lone used squares form their own
+    isolated regions, while several adjacent squares all count as a single region.
+
+    In the example above, the following nine regions are visible, each marked with a distinct digit:
+
+    11.2.3..-->
+    .1.2.3.4
+    ....5.6.
+    7.8.55.9
+    .88.5...
+    88..5..8
+    .8...8..
+    88.8.88.-->
+    |      |
+    V      V
+
+    Of particular interest is the region marked 8; while it does not appear contiguous in this small view, all of
+    the squares marked 8 are connected when considering the whole 128x128 grid. In total, in this example, 1242
+    regions are present.
+
+    How many regions are present given your key string?
     """
     pass
 
+
+INPUT = 'wenycdww'
+
+
+def scramble(data, length_list, position=0, skip=0):
+    tmp = list()
+    for length in length_list:
+        for i in range(length):
+            tmp.append(data[(position + i) % len(data)])
+        while len(tmp) > 0:
+            data[position % len(data)] = tmp.pop()
+            position += 1
+        position = position + skip % len(data)
+        skip += 1
+    return data, position, skip
+
+
+def encode(string):
+    lengths = [ord(c) for c in string]
+    lengths.extend([17, 31, 73, 47, 23])
+    return lengths
+
+
+def test_encode():
+    assert encode('1,2,3') == [49, 44, 50, 44, 51, 17, 31, 73, 47, 23]
+
+
+def knot_hash(string, as_binary=False):
+    lengths = encode(string)
+    data, position, skip = list(range(256)), 0, 0
+    for _ in range(64):
+        data, position, skip = scramble(data, lengths, position, skip)
+    tmp = 0
+    result = []
+    data.append(0)
+    for i, d in enumerate(data):
+        if i > 0 and i % 16 == 0:
+            if as_binary:
+                string = bin(int(hex(256+tmp)[-2:], 16))[2:]
+                result.append(string.rjust(8, '0'))
+            else:
+                result.append(hex(256+tmp)[-2:])
+            tmp = 0
+        tmp ^= d
+    return ''.join(result)
+
+
+def test_knot_hash():
+    assert knot_hash('') == 'a2582a3a0e66e6e86e3812dcb672a272'
+    assert knot_hash('AoC 2017') == '33efeb34ea91902bb2f59c9920caa6cd'
+    assert knot_hash('1,2,3') == '3efbe78a8d82f29979031a4aa0b16a9d'
+    assert knot_hash('1,2,4') == '63960835bcdc130f0b66d7ff4f6a5a8e'
+    # new stuff
+    assert knot_hash('flqrgnkx-0', as_binary=True)[0:8] == '11010100'
+    assert knot_hash('flqrgnkx-1', as_binary=True)[0:8] == '01010101'
+    assert knot_hash('flqrgnkx-2', as_binary=True)[0:8] == '00001010'
+    assert knot_hash('flqrgnkx-3', as_binary=True)[0:8] == '10101101'
+    assert knot_hash('flqrgnkx-4', as_binary=True)[0:8] == '01101000'
+    assert knot_hash('flqrgnkx-5', as_binary=True)[0:8] == '11001001'
+    assert knot_hash('flqrgnkx-6', as_binary=True)[0:8] == '01000100'
+    assert knot_hash('flqrgnkx-7', as_binary=True)[0:8] == '11010110'
+
+
+class Pt(NamedTuple):
+    x: int
+    y: int
+
+    def __add__(self, other):
+        return Pt(self.x + other.x, self.y + other.y)
+
+
+class Disk:
+    def __init__(self, key_string):
+        self.ones = set()
+        self.components = {}
+
+        for y in range(128):
+            row = knot_hash(f'{key_string}-{y}', as_binary=True)
+            for x, c in enumerate(row):
+                if c == '1':
+                    self.ones.add(Pt(x, y))
+
+    def num_of_components(self):
+        component = 0
+        pt_as_component = {p: 0 for p in self.ones}
+        for p in self.ones:
+            if pt_as_component[p] == 0:
+                component += 1
+                component_boundary = [p]
+                while len(component_boundary) > 0:
+                    explore_pt = component_boundary.pop()
+                    pt_as_component[explore_pt] = component
+                    for d in [Pt(-1, 0), Pt(1, 0), Pt(0, 1), Pt(0, -1)]:
+                        new_p = explore_pt + d
+                        if new_p in pt_as_component and pt_as_component[new_p] == 0:
+                            component_boundary.append(new_p)
+        self.components = pt_as_component
+        return component
+
+
+def test_calculate_grid():
+    # sample_disk = Disk('flqrgnkx')
+    # assert len(sample_disk) == 8108
+    puzzle_disk = Disk(INPUT)
+    assert len(puzzle_disk.ones) == 8226
+    assert puzzle_disk.num_of_components() == 1128
