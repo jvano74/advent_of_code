d938a4c603d7537d34c1371a547d35f90ef0f771
John Vano <jvano74@gmail.com>
2021-01-17 00:18:47 -0800

    AoC 2018 day 15

--- 2018/day_15_test.py
+++ 2018/day_15_test.py
@@ -1,3 +1,7 @@
+from typing import NamedTuple
+from queue import PriorityQueue
+
+
 class Puzzle:
     """
     --- Day 15: Beverage Bandits ---
@@ -322,6 +326,372 @@ class Puzzle:
     Outcome: 20 * 937 = 18740
 
     What is the outcome of the combat described in your puzzle input?
+
+    --- Part Two ---
+    According to your calculations, the Elves are going to lose badly. Surely, you won't mess up the timeline too
+    much if you give them just a little advanced technology, right?
+
+    You need to make sure the Elves not only win, but also suffer no losses: even the death of a single Elf is
+    unacceptable.
+
+    However, you can't go too far: larger changes will be more likely to permanently alter spacetime.
+
+    So, you need to find the outcome of the battle in which the Elves have the lowest integer attack power (at least 4)
+    that allows them to win without a single death. The Goblins always have an attack power of 3.
+
+    In the first summarized example above, the lowest attack power the Elves need to win without losses is 15:
+
+    #######       #######
+    #.G...#       #..E..#   E(158)
+    #...EG#       #...E.#   E(14)
+    #.#.#G#  -->  #.#.#.#
+    #..G#E#       #...#.#
+    #.....#       #.....#
+    #######       #######
+
+    Combat ends after 29 full rounds
+    Elves win with 172 total hit points left
+    Outcome: 29 * 172 = 4988
+    In the second example above, the Elves need only 4 attack power:
+
+    #######       #######
+    #E..EG#       #.E.E.#   E(200), E(23)
+    #.#G.E#       #.#E..#   E(200)
+    #E.##E#  -->  #E.##E#   E(125), E(200)
+    #G..#.#       #.E.#.#   E(200)
+    #..E#.#       #...#.#
+    #######       #######
+
+    Combat ends after 33 full rounds
+    Elves win with 948 total hit points left
+    Outcome: 33 * 948 = 31284
+    In the third example above, the Elves need 15 attack power:
+
+    #######       #######
+    #E.G#.#       #.E.#.#   E(8)
+    #.#G..#       #.#E..#   E(86)
+    #G.#.G#  -->  #..#..#
+    #G..#.#       #...#.#
+    #...E.#       #.....#
+    #######       #######
+
+    Combat ends after 37 full rounds
+    Elves win with 94 total hit points left
+    Outcome: 37 * 94 = 3478
+    In the fourth example above, the Elves need 12 attack power:
+
+    #######       #######
+    #.E...#       #...E.#   E(14)
+    #.#..G#       #.#..E#   E(152)
+    #.###.#  -->  #.###.#
+    #E#G#G#       #.#.#.#
+    #...#G#       #...#.#
+    #######       #######
+
+    Combat ends after 39 full rounds
+    Elves win with 166 total hit points left
+    Outcome: 39 * 166 = 6474
+    In the last example above, the lone Elf needs 34 attack power:
+
+    #########       #########
+    #G......#       #.......#
+    #.E.#...#       #.E.#...#   E(38)
+    #..##..G#       #..##...#
+    #...##..#  -->  #...##..#
+    #...#...#       #...#...#
+    #.G...G.#       #.......#
+    #.....G.#       #.......#
+    #########       #########
+
+    Combat ends after 30 full rounds
+    Elves win with 38 total hit points left
+    Outcome: 30 * 38 = 1140
+    After increasing the Elves' attack power until it is just barely enough for them to win without any Elves dying,
+    what is the outcome of the combat described in your puzzle input?
     """
     pass
 
+
+with open('day_15_input.txt') as fp:
+    INPUTS = [line.strip() for line in fp]
+
+
+class Pt(NamedTuple):
+    x: int
+    y: int
+
+    def __add__(self, other):
+        return Pt(self.x + other.x, self.y + other.y)
+
+    def __lt__(self, other):
+        """
+        Update ordering to use 'reading order' with y value 1st then x value
+        """
+        if self.y < other.y:
+            return True
+        if self.y > other.y:
+            return False
+        if self.x < other.x:
+            return True
+        return False
+
+
+def test_pt():
+    assert Pt(5, 1) < Pt(3, 2)
+    assert not Pt(5, 4) < Pt(3, 2)
+    assert Pt(1, 2) < Pt(3, 2)
+
+
+class Unit(NamedTuple):
+    hit_points: int
+    attack_power: int
+
+    @staticmethod
+    def new(attack_power=3):
+        return Unit(hit_points=200, attack_power=attack_power)
+
+    def attacked_by(self, opponent):
+        new_hp = self.hit_points - opponent.attack_power
+        if new_hp <= 0:
+            return None
+        return Unit(hit_points=new_hp, attack_power=self.attack_power)
+
+
+class Board:
+    def __init__(self, raw_board, super_powered_elfs=None):
+        self.grid = set()
+        self.elfs = {}
+        self.super_powered_elfs = super_powered_elfs
+        self.goblins = {}
+        self.max_pt = Pt(0, 0)
+
+        for y, raw_line in enumerate(raw_board):
+            for x, c in enumerate(raw_line):
+                pt = Pt(x, y)
+                if self.max_pt < pt:
+                    self.max_pt = pt
+                if c != '#':
+                    self.grid.add(pt)
+                if c == 'E':
+                    if self.super_powered_elfs is None:
+                        self.elfs[pt] = Unit.new()
+                    else:
+                        self.elfs[pt] = Unit.new(super_powered_elfs)
+                elif c == 'G':
+                    self.goblins[pt] = Unit.new()
+        self.initial_elf_count = len(self.elfs)
+        self.initial_goblin_count = len(self.goblins)
+
+    def print_state(self):
+        results = []
+        for y in range(self.max_pt.y + 1):
+            line = []
+            stats = []
+            for x in range(self.max_pt.x + 1):
+                pt = Pt(x, y)
+                if pt not in self.grid:
+                    line.append('#')
+                elif pt in self.elfs:
+                    line.append('E')
+                    stats.append(f'E({self.elfs[pt].hit_points})')
+                elif pt in self.goblins:
+                    line.append('G')
+                    stats.append(f'G({self.goblins[pt].hit_points})')
+                else:
+                    line.append('.')
+            results.append(f"{''.join(line)}  {', '.join(stats)}")
+        print('\n'.join(results))
+
+    def neighbors(self, pt, obstacles=None):
+        if obstacles is None:
+            obstacles = set()
+        results = []
+        for delta in [Pt(1, 0), Pt(-1, 0), Pt(0, 1), Pt(0, -1)]:
+            new_pt = pt + delta
+            if new_pt in self.grid and new_pt not in obstacles:
+                results.append(pt + delta)
+        return results
+
+    def find_nearest_goal(self, pt, obstacles, goals):
+        boundary = PriorityQueue()
+        boundary.put((0, pt, None))
+        visited = {pt}
+        max_distance = None
+        possible_paths = set()
+        while not boundary.empty():
+            distance, current_pt, first_step = boundary.get()
+            if max_distance is not None and distance > max_distance:
+                pass  # too far
+            elif current_pt in goals:
+                # return distance, current_pt, first_step
+                possible_paths.add((distance, current_pt, first_step))
+                max_distance = distance
+            else:
+                for potential in self.neighbors(current_pt, obstacles | visited):
+                    if first_step is None:
+                        boundary.put((distance + 1, potential, potential))
+                    else:
+                        boundary.put((distance + 1, potential, first_step))
+                    visited.add(potential)
+        if len(possible_paths) > 0:
+            sorted_goals = sorted(possible_paths)
+            return sorted_goals[0]
+        return -1, None, None
+
+    def take_turn(self, pt, team, opposition):
+        if pt not in team:  # should not happen
+            return False
+        no_opponents = False if len(opposition) > 0 else True
+        dist, target, first_step = self.find_nearest_goal(pt, set(team.keys()), set(opposition.keys()))
+
+        if dist > 1:  # move toward attack
+            team[first_step] = team.pop(pt)
+            pt = first_step
+            dist -= 1
+
+        if dist == 1:  # attack
+            # could be multiple opponents in range to need to check target
+            targets = [(opposition[pt + d].hit_points, pt + d)
+                       for d in [Pt(1, 0), Pt(-1, 0), Pt(0, 1),  Pt(0, -1)]
+                       if pt + d in opposition]
+            _, new_target = min(targets)
+            opponent = opposition.pop(new_target)
+            new_opponent = opponent.attacked_by(team[pt])
+            if new_opponent is not None:
+                opposition[new_target] = new_opponent
+
+        return no_opponents
+
+    def run(self, print_round=False):
+        rounds = 0
+        no_opponents = False
+        if print_round:
+            print()
+            print(f'Initially:')
+            self.print_state()
+        while len(self.elfs) > 0 and len(self.goblins) > 0:
+            if self.super_powered_elfs is not None and self.initial_elf_count > len(self.elfs):
+                return -1, 0
+            units = []
+            for pt in self.elfs:
+                units.append((pt, 'e'))
+            for pt in self.goblins:
+                units.append((pt, 'g'))
+            for pt, unit_type in sorted(units):
+                if unit_type == 'e' and pt in self.elfs:
+                    no_opponents = self.take_turn(pt, self.elfs, self.goblins)
+                elif unit_type == 'g' and pt in self.goblins:
+                    no_opponents = self.take_turn(pt, self.goblins, self.elfs)
+                if no_opponents:
+                    break
+            if no_opponents:
+                break
+            rounds += 1
+            if print_round:
+                print()
+                print(f'After round{"s" if rounds > 0 else ""} {rounds}:')
+                self.print_state()
+        survivors = list(self.goblins.values())
+        survivors.extend(list(self.elfs.values()))
+        return rounds, sum([s.hit_points for s in survivors])
+
+
+SAMPLE_0 = ['#######',
+            '#.G...#',
+            '#...EG#',
+            '#.#.#G#',
+            '#..G#E#',
+            '#.....#',
+            '#######']
+
+RESULT_0 = 47, 590
+
+SAMPLE_1 = ['#######',
+            '#G..#E#',
+            '#E#E.E#',
+            '#G.##.#',
+            '#...#E#',
+            '#...E.#',
+            '#######']
+
+RESULT_1 = 37, 982
+
+SAMPLE_2 = ['#######',
+            '#E..EG#',
+            '#.#G.E#',
+            '#E.##E#',
+            '#G..#.#',
+            '#..E#.#',
+            '#######']
+
+RESULT_2 = 46, 859
+
+SAMPLE_3 = ['#######',
+            '#E.G#.#',
+            '#.#G..#',
+            '#G.#.G#',
+            '#G..#.#',
+            '#...E.#',
+            '#######']
+
+RESULT_3 = 35, 793
+
+SAMPLE_4 = ['#######',
+            '#.E...#',
+            '#.#..G#',
+            '#.###.#',
+            '#E#G#G#',
+            '#...#G#',
+            '#######']
+
+RESULT_4 = 54, 536
+
+SAMPLE_5 = ['#########',
+            '#G......#',
+            '#.E.#...#',
+            '#..##..G#',
+            '#...##..#',
+            '#...#...#',
+            '#.G...G.#',
+            '#.....G.#',
+            '#########']
+
+RESULT_5 = 20, 937
+
+
+def test_board():
+    board = Board(SAMPLE_0)
+    observed = board.run(print_round=False)
+    assert observed == RESULT_0
+
+    board = Board(SAMPLE_1)
+    assert board.run() == RESULT_1
+
+    board = Board(SAMPLE_2)
+    assert board.run() == RESULT_2
+
+    board = Board(SAMPLE_3)
+    assert board.run() == RESULT_3
+
+    board = Board(SAMPLE_4)
+    assert board.run() == RESULT_4
+
+    board = Board(SAMPLE_5)
+    assert board.run() == RESULT_5
+
+
+def test_puzzle_pt1_board():
+    board = Board(INPUTS)
+    assert board.run() == (71, 2656)
+    assert 71 * 2656 == 188576
+
+
+def test_puzzle_pt2_board():
+    for super_power in range(4, 100):
+        board = Board(INPUTS, super_powered_elfs=super_power)
+        result = board.run()
+        if result[0] != -1:
+            break
+    assert super_power == 15
+    assert result == (44, 1298)
+    assert 44 * 1298 == 57112
