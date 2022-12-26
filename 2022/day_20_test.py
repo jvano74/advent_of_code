c98f73eb3a1121f32d5ee14352802a06189ed033
John Vano <jvano74@gmail.com>
2022-12-26 14:17:45 -0800

    AoC 2022 day 20

--- 2022/day_20_test.py
+++ 2022/day_20_test.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Puzzle:
     """
     --- Day 20: Grove Positioning System ---
@@ -76,6 +79,63 @@ class Puzzle:
     Mix your encrypted file exactly once. What is the sum of the three numbers
     that form the grove coordinates?
 
+    --- Part Two ---
+    The grove coordinate values seem nonsensical. While you ponder the mysteries
+    of Elf encryption, you suddenly remember the rest of the decryption routine
+    you overheard back at camp.
+
+    First, you need to apply the decryption key, 811589153. Multiply each number
+    by the decryption key before you begin; this will produce the actual list
+    of numbers to mix.
+
+    Second, you need to mix the list of numbers ten times. The order in which
+    the numbers are mixed does not change during mixing; the numbers are still
+    moved in the order they appeared in the original, pre-mixed list. (So, if
+    -3 appears fourth in the original list of numbers to mix, -3 will be the
+    fourth number to move during each round of mixing.)
+
+    Using the same example as above:
+
+    Initial arrangement:
+    811589153, 1623178306, -2434767459, 2434767459, -1623178306, 0, 3246356612
+
+    After 1 round of mixing:
+    0, -2434767459, 3246356612, -1623178306, 2434767459, 1623178306, 811589153
+
+    After 2 rounds of mixing:
+    0, 2434767459, 1623178306, 3246356612, -2434767459, -1623178306, 811589153
+
+    After 3 rounds of mixing:
+    0, 811589153, 2434767459, 3246356612, 1623178306, -1623178306, -2434767459
+
+    After 4 rounds of mixing:
+    0, 1623178306, -2434767459, 811589153, 2434767459, 3246356612, -1623178306
+
+    After 5 rounds of mixing:
+    0, 811589153, -1623178306, 1623178306, -2434767459, 3246356612, 2434767459
+
+    After 6 rounds of mixing:
+    0, 811589153, -1623178306, 3246356612, -2434767459, 1623178306, 2434767459
+
+    After 7 rounds of mixing:
+    0, -2434767459, 2434767459, 1623178306, -1623178306, 811589153, 3246356612
+
+    After 8 rounds of mixing:
+    0, 1623178306, 3246356612, 811589153, -2434767459, 2434767459, -1623178306
+
+    After 9 rounds of mixing:
+    0, 811589153, 1623178306, -2434767459, 3246356612, 2434767459, -1623178306
+
+    After 10 rounds of mixing:
+    0, -2434767459, 1623178306, 3246356612, -1623178306, 2434767459, 811589153
+
+    The grove coordinates can still be found in the same way. Here, the 1000th
+    number after 0 is 811589153, the 2000th is 2434767459, and the 3000th is
+    -1623178306; adding these together produces 1623178306.
+
+    Apply the decryption key and mix your encrypted file ten times. What is the
+    sum of the three numbers that form the grove coordinates?
+
     """
 
 
@@ -86,3 +146,36 @@ with open("day_20_input.txt") as fp:
     MY_INPUT = [int(line) for line in fp]
 
 
+class CoordinateFile:
+    def __init__(self, raw_encrypted, decryption_key=1, levels=1) -> None:
+        self.raw_encrypted = raw_encrypted
+        working = deque((n, v * decryption_key) for n, v in enumerate(raw_encrypted))
+        modulus = len(working)
+        for _ in range(levels):
+            for n in range(len(raw_encrypted)):
+                while working[0][0] != n:
+                    working.rotate(1)
+                move = working.popleft()
+                working.rotate(-move[1] % (modulus - 1))
+                working.append(move)
+        while working[0][1] != 0:
+            working.rotate(1)
+        self.processed = working
+
+    def get_groove_coordinates(self):
+        modulus = len(self.processed)
+        return sum(self.processed[n % modulus][1] for n in [1000, 2000, 3000])
+
+
+def test_coordinate_file():
+    sample = CoordinateFile(SAMPLE)
+    assert sample.get_groove_coordinates() == 3
+    sample = CoordinateFile(SAMPLE, decryption_key=811589153, levels=10)
+    assert sample.get_groove_coordinates() == 1623178306
+
+
+def test_coordinate_file():
+    my_input = CoordinateFile(MY_INPUT)
+    assert my_input.get_groove_coordinates() == 3466
+    my_input = CoordinateFile(MY_INPUT, decryption_key=811589153, levels=10)
+    assert my_input.get_groove_coordinates() == 9995532008348
