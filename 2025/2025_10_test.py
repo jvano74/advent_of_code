a84439cd3c14e7fa0c6f0ed012bb37d838618bcc
John Vano <john.vano@umm.edu>
2025-12-24 09:31:58 -0800

    A0C 2025 day 10 part 2

--- 2025/2025_10_test.py
+++ 2025/2025_10_test.py
@@ -1,7 +1,12 @@
+import sympy as sp
+import numpy as np
+
+from sympy.core.numbers import int_valued
+from scipy.optimize import milp, LinearConstraint
+
 from pathlib import Path
 from typing import List
 from itertools import combinations
-from collections import defaultdict
 
 
 class Puzzle:
@@ -156,6 +161,9 @@ class Puzzle:
     What is the fewest button presses required to correctly configure the
     joltage level counters on all of the machines?
 
+    Your puzzle answer was 15688.
+
+    Both parts of this puzzle are complete! They provide two gold stars: **
     """
 
 
@@ -182,12 +190,20 @@ class Machine:
         self.target = sum(2**p if d == "#" else 0 for p, d in enumerate(state[1:-1]))
 
         self.buttons = []
+        rows = []
+
         while raw_machine:
             element = raw_machine.pop(0)
             if element[0] != "(":
                 raise Exception("something went wrong with input data")
-            change = sum(2 ** int(d) for d in element[1:-1].split(","))
-            self.buttons.append(change)
+            element = [int(d) for d in element[1:-1].split(",")]
+            self.buttons.append(sum(2**d for d in element))
+            row = [1 if d in element else 0 for d, _ in enumerate(self.joltage)]
+            rows.append(row)
+
+        self.matrix = sp.Matrix(rows).T
+        # self.np_matrix = np.array(rows, dtype=float).T
+        self.np_matrix = np.array(rows, dtype=int).T
 
     def min_on(self):
         for n in range(1, len(self.buttons) + 1):
@@ -199,6 +215,90 @@ class Machine:
                     return n
         raise Exception("could not turn on")
 
+    def np_find_joltage(self):
+        A = self.np_matrix
+        b = np.array(self.joltage, dtype=int).flatten()
+        integrality = np.ones(A.shape[1])
+        constraints = LinearConstraint(A, b, b)
+        c = np.ones(A.shape[1])
+        res = milp(c=c, constraints=constraints, integrality=integrality)
+        return res.fun
+
+    def find_joltage(self):
+        M = self.matrix
+        b = sp.Matrix(self.joltage)
+        vars = sp.symbols(f"x0:{M.cols}")
+        solutions = sp.linsolve((M, b), vars)
+        solution = list(solutions)[0]
+        # ----------------
+        # SAMPLE[0]
+        # (-x3 + x5 + 2, 5 - x5, -x3 + x5 + 1, x3, 3 - x5, x5)
+        # -x3 + x5 + 2 -> 1
+        #     - x5 + 5 -> 2
+        # -x3 + x5 + 1 -> 0
+        #  x3          -> 4
+        #     - x5 + 3 -> 0
+        #       x5     -> 3
+        # ----------------
+        #  x5 = 0, 1, 2, 3
+        #  x3 = 1, 2, 3, 4
+        # ----------------
+        # SAMPLE[1]
+        # (2 - x2, x2 + 5, x2, 5, 0)
+        # -x2 + 2 -> 2
+        #  x2 + 5 -> 5
+        #  x2     -> 0
+        #       5 -> 5
+        #       0 -> 0
+        # ----------------
+        # SAMPLE[2]
+        # (6 - x3, x3 - 1, 5, x3)
+        # -x3 + 6 -> 5
+        #  x3 - 1 -> 0
+        #       5 -> 5
+        #  x3     -> 1
+        # ----------------
+        # Non integer example
+        # -x11/3 + 7*x12/3 + x7 - 634/3
+        # 4*x11/3 - 10*x12/3 - x7 + 670/3
+        # -x11 + x12 + 8
+        # -2*x11/3 - x12/3 - x7 + 634/3
+        # x11/3 - x12/3 + 37/3
+        # x12 - 12
+        # -2*x11/3 - 4*x12/3 + 112/3, x7
+        # x11/3 - 4*x12/3 + 91/3
+        # x11/3 + 2*x12/3 - 5/3
+        # 2
+        # x11
+        # x12
+
+        val_to_minimize = sum(solution)
+        constraints = [eq >= 0 for eq in solution]
+
+        ans = sp.solvers.simplex.lpmin(val_to_minimize, constraints)
+        if int_valued(ans[0]) and all(int_valued(val) for val in ans[1].values()):
+            return ans
+
+        # try again with scipy solver
+        all_symbols = set().union(*(eq.free_symbols for eq in solution))
+        vars_list = sorted(list(all_symbols), key=lambda s: s.name)
+        integrality = np.ones(len(vars_list))
+
+        A_sym, b_sym = sp.linear_eq_to_matrix(solution, vars_list)
+        A = np.array(A_sym.tolist(), dtype=float)
+        b_l = np.array(b_sym.tolist(), dtype=float).flatten()
+        b_u = np.full_like(b_l, np.inf)
+        constraints = LinearConstraint(A, b_l, b_u)
+
+        c_sym, c_sym_ofset = sp.linear_eq_to_matrix(val_to_minimize, vars_list)
+        c = np.array(c_sym.tolist(), dtype=float).flatten()
+        c_ofset = c_sym_ofset[0, 0]
+        res = milp(c=c, constraints=constraints, integrality=integrality)
+        if not int_valued(c_ofset):
+            return -1, c_ofset
+        c_int = np.round(res.fun).astype(int) + c_ofset
+        return c_int, "via_milp"
+
 
 class Factory:
 
@@ -208,19 +308,39 @@ class Factory:
     def total_min(self):
         return sum(machine.min_on() for machine in self.machines)
 
+    def np_total_joltage(self):
+        return sum(machine.np_find_joltage() for machine in self.machines)
+
+    def total_joltage(self):
+        total = 0
+        non_int_machines = []
+        for machine in self.machines:
+            ans = machine.find_joltage()
+            if ans[0] == -1:
+                non_int_machines.append((machine, ans))
+            else:
+                total += ans[0]
+        return total
+
 
 def test_machine():
     sample_machine = Machine(SAMPLE[0])
     assert sample_machine.min_on() == 2
+    assert sample_machine.find_joltage()[0] == 10
     sample_machine = Machine(SAMPLE[1])
     assert sample_machine.min_on() == 3
+    assert sample_machine.find_joltage()[0] == 12
     sample_machine = Machine(SAMPLE[2])
     assert sample_machine.min_on() == 2
+    assert sample_machine.find_joltage()[0] == 11
 
 
 def test_factory():
     sample_factory = Factory(SAMPLE)
     assert sample_factory.total_min() == 7
+    assert sample_factory.total_joltage() == 33
+    assert sample_factory.np_total_joltage() == 33
 
     my_factory = Factory(RAW_INPUT)
     assert my_factory.total_min() == 396
+    assert my_factory.np_total_joltage() == 15688.0
